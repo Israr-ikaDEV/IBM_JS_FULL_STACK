2145f36e0ea22d7d200305567521b1bd674c3deb
ikaDEV <kingisrar.ika@gmail.com>
2025-09-30 20:12:30 +0500

    lifeCycle of a component

--- VITE-react/vite-project/src/main.jsx
+++ VITE-react/vite-project/src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import FunctionalComp from './FunctionalComp'
-
+import LifecycleClassComponent from './LifecycleClassComponent'
 const names = ['Farhan', 'Babar', 'Fakhar', 'Hassan', 'Nafay','Rizwan','Waseem', 'Naseem','Shaheen','Sufyan','Abrar']
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     {names.map((name, index) => (
       <FunctionalComp key={index} name={name} />
     ))}
-  </StrictMode>,
+    <LifecycleClassComponent />
+
+  </>
 )
+
+
